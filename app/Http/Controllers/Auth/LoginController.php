f96a3a2b460a10c3f778c4e08eeb72fb5a161a7c
nana <chyhyy76@users.noreply.github.com>
2021-09-28 10:34:10 +0900

    first commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -44,7 +44,7 @@ class LoginController extends Controller
         }
     }
 
-    use AuthenticatesUsers;
+    //use AuthenticatesUsers;
 
     /**
      * Where to redirect users after login.
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Operate\Members\MembersController;
 use Illuminate\Support\Facades\Route;
 
 /*
