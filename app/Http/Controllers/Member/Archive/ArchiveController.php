5ee65e3a54e20e39d62c1679fe64918313a4e7f3
kouheihayashi78 <kouhei.basketball7.8@gmail.com>
2021-12-06 17:53:05 +0900

    少し修正

--- app/Http/Controllers/Member/Archive/ArchiveController.php
+++ app/Http/Controllers/Member/Archive/ArchiveController.php
@@ -2,13 +2,8 @@
 
 namespace App\Http\Controllers\Member\Archive;
 
-<<<<<<< HEAD
-
-use App\Http\Controllers\Controller;
-=======
 use App\Http\Controllers\Controller;
 
->>>>>>> d02ff4a421edd577d093ef54df1284157fea653c
 use Illuminate\Http\Request;
 
 class ArchiveController extends Controller
